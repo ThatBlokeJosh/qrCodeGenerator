4d6bd4fd2913d539cae0270827771806470bbaf3
Joshua Thomas Remington <106017903+ThatBlokeJosh@users.noreply.github.com>
2022-11-12 17:58:02 +0100

    Update qrgenerator.py

--- qrgenerator.py
+++ qrgenerator.py
@@ -1,11 +1,11 @@
-import qrcode
+import qrcode # Import the qrcode library
 
 
 print("Welcome to QR Code Generator")
 
-qr_name = input("Enter a valid URL: ")
+qr_name = input("Enter a valid URL: ") 
 
-img = qrcode.make(qr_name)
+img = qrcode.make(qr_name) # Make a qr code from the qr_name variable
 
 type(img)
 
@@ -16,6 +16,6 @@ valid_formats = ['png', 'jpg', 'jpeg', 'bmp', 'gif']
 format = input("Enter a valid format: ").lower()
 
 if format in valid_formats:
-    img.save(f"{image_name}.{format}")
+    img.save(f"{image_name}.{format}") # Create the image
 else:
     print("Invalid format")
